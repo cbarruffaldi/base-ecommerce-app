8a00013f3a95462e03eed6b51a75ce1955ed0606
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2021-10-12 19:59:18 +0200

    Slider component

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/1462637/pexels-photo-1462637.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/1126993/pexels-photo-1126993.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/6599084/pexels-photo-6599084.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
