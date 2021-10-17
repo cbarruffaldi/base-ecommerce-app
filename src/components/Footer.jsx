ed04721bd2e4c2fb0f785efb8e0477f8c463b4e4
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2021-10-17 17:37:01 +0200

    Responsive Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,12 @@
 import { Facebook, Pinterest, Twitter, Instagram, Room, Phone, MailOutline } from "@material-ui/icons"
 import styled from "styled-components"
+import { mobile } from "../responsive";
 
 const Container = styled.div`
     display: flex;
+    ${mobile({ 
+        flexDirection: "column"
+     })}
 `;
 const Left = styled.div`
     flex: 1;
@@ -37,6 +41,9 @@ const SocialIcon = styled.div`
 const Center = styled.div`
     flex: 1;
     padding: 20px;
+    ${mobile({ 
+        display: "none"
+     })}
 `;
 
 const Title = styled.h3`
@@ -59,6 +66,9 @@ const ListItem = styled.li`
 const Right = styled.div`
     flex: 1;
     padding: 20px;
+    ${mobile({ 
+        backgroundColor: "#fff8f8"
+     })}
 `;
 
 const ContactItem = styled.div`
