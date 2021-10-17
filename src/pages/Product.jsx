4d5890304be9e9764b137e334407f7938b1a7542
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2021-10-17 17:47:43 +0200

    responsive product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -4,6 +4,7 @@ import Announcement from "../components/Announcement";
 import Newsletter from "../components/Newsletter";
 import Footer from "../components/Footer";
 import { Add, Remove } from "@material-ui/icons";
+import { mobile } from "../responsive";
 
 
 const Container = styled.div``;
@@ -11,6 +12,10 @@ const Container = styled.div``;
 const Wrapper = styled.div`
     padding: 50px;
     display: flex;
+    ${mobile({ 
+        flexDirection: "column",
+        padding: "10px"
+     })}
 `;
 
 const ImgContainer = styled.div`
@@ -21,11 +26,17 @@ const Image = styled.img`
     width: 100%;
     height: 80vh;
     object-fit: cover;
+    ${mobile({ 
+        height: "40vh"
+     })}
 `;
 
 const InfoContainer = styled.div`
     flex: 1;
     padding: 0px 50px;
+    ${mobile({ 
+        padding: "10px"
+     })}
 `;
 
 const Title = styled.h1`
@@ -46,6 +57,9 @@ const FilterContainer = styled.div`
     margin: 30px 0px;
     display: flex;
     justify-content: space-between;
+    ${mobile({ 
+        width: "100%"
+     })}
 `;
 
 const Filter = styled.div`
@@ -79,6 +93,9 @@ const AddContainer = styled.div`
     align-items: center;
     justify-content: space-between;
     width: 50%;
+    ${mobile({ 
+        width: "100%"
+     })}
 `;
 
 const AmountContainer = styled.div`
