54402dbee038efd27d5da162d8a6fbc7b34576a2
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2021-10-17 17:29:22 +0200

    Responsive Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,13 @@ import { Badge } from '@material-ui/core'
 import { Search, ShoppingCartOutlined } from '@material-ui/icons'
 import React from 'react'
 import styled from 'styled-components'
+import { mobile } from '../responsive'
 
 const Container = styled.div`
     height: 60px;
+    ${mobile({ 
+        height: "50px"
+     })}
 `
 
 const Wrapper = styled.div`
@@ -12,6 +16,10 @@ const Wrapper = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
+
+    ${mobile({ 
+        padding: "10px 0px"
+     })}
 `
 const Left = styled.div`
     flex:1;
@@ -24,6 +32,9 @@ const Center = styled.div`
 `
 const Logo = styled.h1`
     font-weight: bold;
+    ${mobile({ 
+        fontSize: "16px"
+     })}
 `
 
 const Right = styled.div`
@@ -31,11 +42,18 @@ const Right = styled.div`
     display: flex;
     align-items: center;
     justify-content: flex-end;
+    ${mobile({ 
+        flex: 2,
+        justifyContent: "center"
+     })}
 `
 
 const Language = styled.span`
     font-size: 14px;
-    curson: pointer
+    cursor: pointer;
+    ${mobile({ 
+        display: "none"
+     })}
 `
 
 const SearchContainer = styled.div`
@@ -48,12 +66,19 @@ const SearchContainer = styled.div`
 
 const Input = styled.input`
     border: None;
+    ${mobile({ 
+        width: "50px"
+     })}
 `
 
 const MenuItem = styled.div`
     font-size: 14px;
     cursor: pointer;
     margin-left: 25px;
+    ${mobile({ 
+        fontSize: "12px",
+        marginLeft: "10px"
+     })}
 `
 
 function Navbar() {
@@ -63,7 +88,7 @@ function Navbar() {
                 <Left>
                     <Language>EN</Language>
                     <SearchContainer>
-                        <Input></Input>
+                        <Input placeholder="Search"></Input>
                         <Search style={{ color: "gray", fontSize: 16 }}></Search>
                     </SearchContainer>
                 </Left>
