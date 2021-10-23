0812dae8de0e827d4906a3d020c71bfa3ca4e843
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2021-10-23 11:17:54 +0200

    react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
 import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -6,7 +12,31 @@ import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 
 const App = () => {
-  return <Home/>;
+  const user = true;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route path="/products/:category">
+          <ProductList/>
+        </Route>
+        <Route path="/product/:id">
+          <Product/>
+        </Route>
+        <Route path="/cart">
+          <Cart/>
+        </Route>
+        <Route path="/login">
+          {user ? <Redirect to="/"/> : <Login />}
+        </Route>
+        <Route path="/register">
+        {user ? <Redirect to="/"/> : <Register />}
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
