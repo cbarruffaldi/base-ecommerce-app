db65b91c551b4c9419dcef0a9e0a80bb1111cccf
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2021-10-26 13:21:29 +0200

    get current user from state

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useSelector } from "react-redux";
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,7 +14,7 @@ import Register from "./pages/Register";
 import Success from "./pages/Success";
 
 const App = () => {
-  const user = true;
+  const user = useSelector(state => state.user.currentUser);
   return (
     <Router>
       <Switch>
