9ee9c092a5b2c3c7d281cf28cdcf056dd168ea48
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2021-10-26 13:14:20 +0200

    login api call

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 import styled from "styled-components";
+import { login } from "../redux/apiCalls";
 import { mobile } from "../responsive";
 
 const Container = styled.div`
@@ -58,14 +61,23 @@ const Link = styled.a`
 
 
 const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const dispatch = useDispatch();
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        login(dispatch, {username, password});
+    }
+
     return (
         <Container>
             <Wrapper>
                 <Title>SIGN IN</Title>
                 <Form>
-                    <Input placeholder="username" />
-                    <Input placeholder="password" />
-                    <Button>LOGIN</Button>
+                    <Input placeholder="username" onChange={(e) => setUsername(e.target.value)}/>
+                    <Input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)}/>
+                    <Button onClick={handleClick}>LOGIN</Button>
                     <Link>DO NOT REMEMBER YOUR PASSWORD?</Link>
                     <Link>CREATE A NEW ACCOUNT</Link>
                 </Form>
