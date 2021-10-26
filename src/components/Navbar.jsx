1cb5e06b9f3348c9eb5c72644e794b33bfe11ae4
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2021-10-26 12:33:13 +0200

    Cart page using state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
-import { Badge } from '@material-ui/core'
-import { Search, ShoppingCartOutlined } from '@material-ui/icons'
-import React from 'react'
-import { useSelector } from 'react-redux'
-import styled from 'styled-components'
-import { mobile } from '../responsive'
+import { Badge } from '@material-ui/core';
+import { Search, ShoppingCartOutlined } from '@material-ui/icons';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+import { mobile } from '../responsive';
+import { Link } from "react-router-dom";
+
 
 const Container = styled.div`
     height: 60px;
@@ -101,11 +103,14 @@ function Navbar() {
                 <Right>
                     <MenuItem>REGISTER</MenuItem>
                     <MenuItem>SIGN IN</MenuItem>
-                    <MenuItem>
-                        <Badge badgeContent={quantity} color="primary">
-                            <ShoppingCartOutlined/>
-                        </Badge>
-                    </MenuItem>
+                    <Link to="/cart">
+                        <MenuItem>
+                            <Badge badgeContent={quantity} color="primary">
+                                <ShoppingCartOutlined/>
+                            </Badge>
+                        </MenuItem>
+                    </Link>
+                    
                 </Right>
             </Wrapper>
         </Container>
