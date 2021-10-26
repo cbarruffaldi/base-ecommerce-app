91fca1295447247af43e8a704df5f699383cfee2
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2021-10-26 13:51:58 +0200

    persist cart

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import {configureStore} from "@reduxjs/toolkit";
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import cartReducer from './cartRedux';
 import userReducer from './userRedux';
 import {
@@ -19,15 +19,13 @@ const persistConfig = {
     storage,
   };
   
+const rootReducer = combineReducers({user: userReducer, cart: cartReducer})
 
-const persistedReducer = persistReducer(persistConfig, userReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 
 export const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-        user: persistedReducer,
-    },
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
