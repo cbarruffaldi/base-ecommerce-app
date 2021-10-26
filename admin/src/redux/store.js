a12e702c2c6b953d0720a32ef25c5b19281028be
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2021-10-26 15:06:57 +0200

    store prepared to add more reducers

--- admin/src/redux/store.js
+++ admin/src/redux/store.js
@@ -18,12 +18,12 @@ const persistConfig = {
   storage,
 };
 
-// const rootReducer = combineReducers({
-//   user: userReducer,
+const rootReducer = combineReducers({
+  user: userReducer
   
-// });
+});
 
-const persistedReducer = persistReducer(persistConfig, userReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
