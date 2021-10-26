1336fb44a8e9380a9ad35efb984f38cf5a84bd53
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2021-10-26 15:03:48 +0200

    get token from local storage

--- admin/src/requestMethods.js
+++ admin/src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL="http://localhost:5000/api/";
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNzJhZTc5MDIxNGQyODQ4ODQ3ZDIyYyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNDk4Mzg0MCwiZXhwIjoxNjM1MjQzMDQwfQ.-4J3hdnNvzGbhTk6iCHlJveliAnwMLW1DCAGjs-ykT8";
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).currentUser).accessToken;
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
