a4a0c68f24182cb16d451ff4d1f0d5e767b64613
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2021-11-01 11:31:02 +0100

    TODO: Sales and cost in home page

--- admin/src/components/featuredInfo/FeaturedInfo.jsx
+++ admin/src/components/featuredInfo/FeaturedInfo.jsx
@@ -18,6 +18,7 @@ export default function FeaturedInfo() {
     };
     getIncome();
   }, []);
+  console.log(income);
 return (
     <div className="featured">
       <div className="featuredItem">
@@ -37,9 +38,9 @@ return (
       <div className="featuredItem">
         <span className="featuredTitle">Sales</span>
         <div className="featuredMoneyContainer">
-          <span className="featuredMoney">$4,415</span>
+          <span className="featuredMoney">$-</span>
           <span className="featuredMoneyRate">
-            -1.4 <ArrowDownward className="featuredIcon negative"/>
+            - <ArrowDownward className="featuredIcon negative"/>
           </span>
         </div>
         <span className="featuredSub">Compared to last month</span>
@@ -47,9 +48,9 @@ return (
       <div className="featuredItem">
         <span className="featuredTitle">Cost</span>
         <div className="featuredMoneyContainer">
-          <span className="featuredMoney">$2,225</span>
+          <span className="featuredMoney">$-</span>
           <span className="featuredMoneyRate">
-            +2.4 <ArrowUpward className="featuredIcon"/>
+            + <ArrowUpward className="featuredIcon"/>
           </span>
         </div>
         <span className="featuredSub">Compared to last month</span>
